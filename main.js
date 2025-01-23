f7e80f7f97724e47105935e99f005b4b4c37d326
bhavithran2007 <106386134+bhavithran2007@users.noreply.github.com>
2025-01-23 15:03:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+document.addEventListener("contextmenu",function(event){
+    event.preventDefault();
+    
+},false);
+
+
+document.getElementById("pdfContainer").addEventListener("contextmenu", (e) => {
+    e.preventDefault();
+});
+
+
+
